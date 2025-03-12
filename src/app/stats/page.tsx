0d2b44108c13fcc378e1d3bd15e60a30187239a8
ethan <workoutwrecker@gmail.com>
2025-03-12 11:31:17 +0800

    chore: disable stats temporarily

---
+++ src/app/stats/page.tsx
@@ -0,0 +1,18 @@
+import StatsOverview from '@/components/stats/StatsOverview';
+import Header from '@/components/Header'
+
+export default function StatsPage() {
+  const username = 'misterworker';
+  const repo = 'Portfolio';
+
+  return (
+    <>
+      <Header/>
+        {/* <div className="p-8">
+          <h1 className="text-3xl font-semibold text-gray-100 mb-6 text">GitHub Statistics for {username}/{repo}</h1>
+          <StatsOverview username={username} repo={repo} />
+        </div> */}
+        // TODO: Revive
+    </>
+  );
+}
