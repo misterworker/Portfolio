ea016da8f7f255dae3de077d3b819afd6c78aec3
ethan <workoutwrecker@gmail.com>
2025-03-16 01:21:08 +0800

    chore: add Workout Tracker and workAdvisor

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,7 +6,10 @@ const projects = [
       A mobile application for a startup I co founded: Maibel.ai. This app is aims to be the first app made for women that combines 
       storytelling and personalised AI coaching. I built the app with React Native, allowing for a single codebase to build an app 
       that is compatible with both Android and ios. Expo was also utilised for testing and deploying the application on playstore 
-      and apple store. I made this early version of the application solo.
+      and apple store. Developed Solo. <br />
+      <b>Features</b>: Chatbots, Challenges, Stories, Other bots which facilitate user experience <br />
+      <b>Technologies used</b>: React Native, Expo, Langchain/Langgraph, Google Cloud Run 
+
       `,
       slug: "maibel-ai-app",
       tags: ["mobile", "react-native", "expo"],
@@ -14,20 +17,37 @@ const projects = [
       githubRepo: "https://github.com/misterworker/Maibel-AI-App",
     },
     {
-      title: "Fitness App",
-      description: "A fitness app to track workouts.",
-      slug: "fitness-app",
-      tags: ["mobile", "android", "kotlin"],
-      media: "/images/fitness-app.jpg",
-      githubRepo: "https://github.com/misterworker/fitness-app",
+      title: "WorkAdvisor",
+      description: `
+      A web application that focuses on ensuring that students and professionals get equal opportunities. It features a post predictor, 
+      salary predictor and educational opportunities predictor. I personally worked on the post predictor myself, which predicts post 
+      engagement, which reduces the lack of access to personalised guidance. <br />
+      <b>Features</b>: Fully Connected Neural Network with self scraped and self trained data, AI for ease of use (Automatically suggesting 
+      a revised post title and content), history of post engagements and the ability to restore them, AI filter to prevent abuse and PII leaks.
+      Moreover, there is a page tour using Driver.js, ensuring the user has a grasp on the page features before calling the model.<br />
+      <b>Technologies used</b>: NextJS, Tailwind, Tensorflow, Langchain, BeautifulSoup
+
+      `,
+      slug: "workAdvisor",
+      tags: ["Web", "NextJS", "Deep Learning"],
+      media: ["/projects/workAdvisor/mainPage.jpg", "/projects/workAdvisor/pageTour.jpg", "/projects/workAdvisor/finalPage.jpg"],
+      githubRepo: "https://github.com/misterworker/workAdvisor",
+      website: "https://work-advisor-seven.vercel.app/post-prediction"
     },
     {
-      title: "E-commerce Site",
-      description: "An online shopping platform.",
-      slug: "ecommerce-site",
-      tags: ["Web", "React"],
-      media: "/images/ecommerce.jpg",
-      githubRepo: "",
+      title: "Workout Tracker App",
+      description: `
+      An Android mobile application developed for fitness lovers - Designed for an intuitive and satisfactory way of
+      recording your workouts and analysing your progress metrics. Developed Solo. <br />
+      <b>Features</b>: Theme Switching, Authentication with Firebase, CRUD functions for History, Workouts and Exercises, 
+      Premium features, and Data Visualisations <br />
+      <b>Technologies used</b>: Android Studio, Kotlin, Firebase, Google Cloud Run, Google Play Developer APIs 
+
+      `,
+      slug: "workout-tracker-app",
+      tags: ["mobile", "android", "kotlin"],
+      media: ["/projects/workout_tracker_main.jpg", "/projects/workout_tracker_workouts.jpg", "/projects/workout_tracker_premium.jpg"],
+      githubRepo: "https://github.com/misterworker/WorkoutTracker",
     },
   ];
 
--- src/data/slugProjects.ts
+++ src/data/slugProjects.ts
@@ -10,7 +10,7 @@ const slugProjects = [
       `, desc: "", id: "media-1" },
       { type: 'header', content: 'Creating the AI', desc: "h2", id: "header-1" },
       { type: 'text', content: `
-        An Agent Supervisor handles 4 distinct bots, each well equiped to perform specific tasks.
+        An Agent Supervisor handles 4 distinct bots, each well equipped to perform specific tasks.
         Let's go through<br />
         1. Validation Bot - When it is triggered and what triggers it<br />
         2. Recommendation Bot - What it recommends<br />
@@ -74,7 +74,7 @@ const slugProjects = [
         4. Personality/Personalities<br />
         5. Gender <br />
         The last 4 are set by the user, and passed on to endpoints mentioned earlier. These fields help contextualise the ideal 
-        coach and provide the user with an amazing custom experience. Personalities are based on pre set options, for example 'Analytical'
+        coach and provide the user with an amazing custom experience. Personalities are based on pre-set options, for example 'Analytical'
         or 'Motivating'. This encourages the user to enter a slightly more complex background, although simple ones like "Sarcastic High-
         School Dropout" would still work.
       `, desc: "", id: "media-7" },
@@ -97,65 +97,100 @@ const slugProjects = [
     ],
   },
   {
-    slug: "fitness-app",
+    slug: "workAdvisor",
     media: [
       { type: 'header', content: 'Feature Demonstration', desc: "h2", id: "header-0" },
       { type: "video", content: '/videos/projects/slug/maibel_demo.mp4', desc: "Demo Video", id: "media-1" },
+      { type: 'image', content: '/projects/workAdvisor/plan.jpg', desc: "Plan Overview", id: "media-2" },
       { type: "text", content: `
-        As you can see, the application features an agent that you can speak with, challenge progression based on intent, validation
-        checks, stories, and basic theme switching.
-      `, desc: "", id: "media-2" },
-      { type: 'header', content: 'Creating the AI', desc: "h2", id: "header-3" },
-      { type: 'text', content: `
-        Let's go through<br />
-        1. the validation bot<br />
-        2. challenge recommendation bot<br />
-        3. The main conversational chatbot<br />
-        4. Custom Avatar creation and how that affects the behavior of the chatbot
-      `, desc: "", id: "media-4" },
-      { type: 'header', content: 'Validation Bot', desc: "h3", id: "header-5" },
-      { type: 'header', content: 'Recommendation Bot', desc: "h3", id: "header-6" },
-      { type: 'header', content: 'Conversational Bot', desc: "h3", id: "header-7" },
-      { type: 'header', content: 'Custom Avatars', desc: "h3", id: "header-8" },
-      { type: 'header', content: 'Deployment', desc: "h2", id: "header-9" },
-      { type: 'image', content: '/images/projects/slug/apple_developer_store.jpg', desc: "Apple Developer Store", id: "media-10" },
-      { type: 'image', content: '/images/projects/slug/play_store.jpg', desc: "Google Play Store", id: "media-11" },
-      { type: 'text', content: `
-        As shown above, I've deployed the application for internal testing to a live audience of roughly 15 people in total. These
-        were testers for Maibel's user testing session, and I managed to get the build up in just a week, with no prior experience
-        whatsoever.
-      `, desc: "", id: "media-12" },
+        As you can see, the application features Siew Wei Heng, Lee Yan Da Ethan and Gavin Lim.
+      `, desc: "", id: "media-3" },
+      { 
+        "type": "header", 
+        "content": "Scraping", 
+        "desc": "h2", 
+        "id": "header-1" 
+      },
+      { 
+        "type": "text", 
+        "content": `
+          For the project, I scraped data from <a href="https://www.thestudentroom.co.uk/" target="_blank" class="project-link">The Student Room</a> 
+          using <b>BeautifulSoup</b> and asynchronous requests. Initially, my requests were blocked due to a 
+          <b>403 Forbidden</b> error. To bypass this, I modified the request headers to include a 
+          custom <code>"User-Agent"</code>:<br /><br />
+          
+          <code>"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"</code><br /><br />
+      
+          By using a common browser <code>"User-Agent"</code>, my requests appeared more normal, bypassing the block.<br /><br />
+      
+          After gaining access, I scraped <b>four main categories</b>: A-Levels, GCSEs, Job Experience, and Study Support. 
+          From each category, I extracted <b>post links, number of likes, reply count, and titles</b>. However, I encountered a 
+          major issue - <b>post content was dynamically rendered using JavaScript</b>.<br /><br />
+      
+          Since <b>BeautifulSoup</b> only parses static HTML, I initially considered using <b>Selenium</b> to interact with the JavaScript. 
+          However, given the scale (over <b>100,000 rows</b>), Selenium would have been far too slow. Instead, I analyzed the site's JavaScript 
+          code and used <b>regular expressions (regex)</b> to extract full post content directly from the page source.<br /><br />
+      
+          This approach allowed me to scrape the necessary data <b>efficiently and at scale</b> without needing to render JavaScript.
+        `, 
+        "desc": "", 
+        "id": "media-12" 
+      },
+      { 
+        "type": "header", 
+        "content": "Deep Learning Model", 
+        "desc": "h2", 
+        "id": "header-2" 
+      },
+      { 
+        "type": "text", 
+        "content": `
+          I designed and trained a <b>deep learning model</b> to classify posts using multiple input features, including <b>text, title, numerical data, and categorical data</b>. The architecture consists of:<br /><br />
+      
+          - <b>Text Input</b>: A <code>Bidirectional LSTM</code> processes the main post content.<br />
+          - <b>Title Input</b>: Another <code>Bidirectional LSTM</code> processes the post title separately.<br />
+          - <b>Numerical Input</b>: A single numerical feature (<code>days since post</code>) is included.<br />
+          - <b>Categorical Input</b>: A <code>one-hot encoded</code> category input with 4 features.<br /><br />
+      
+          These inputs are <b>concatenated</b> and passed through dense layers with <b>L2 regularization</b> and <b>dropout</b> to prevent overfitting. The final output layer uses a <b>softmax activation</b> to classify posts into predefined categories.<br /><br />
+      
+          The model is compiled with <b>Adam optimizer</b> and <code>sparse_categorical_crossentropy</code> loss, making it well-suited for multi-class classification.
+        `, 
+        "desc": "", 
+        "id": "media-13" 
+      },
+      { 
+        "type": "header", 
+        "content": "Model Deployment & API", 
+        "desc": "h3", 
+        "id": "header-11" 
+      },
+      { 
+        "type": "text", 
+        "content": `
+          To deploy the deep learning model and make it accessible via API, I used <b>Google Cloud Run</b> with a <b>Dockerized FastAPI application</b>. This setup allows for seamless integration between the model and external services.<br /><br />
+      
+          The API handles:<br />
+          - <b>Post Validation</b>: Ensures content is suitable for public posting.<br />
+          - <b>Post Recommendations</b>: Uses <code>LLMs</code> (GPT-4o-mini & NVIDIA's Llama 3.1) to enhance user engagement.<br />
+          - <b>Response Handling</b>: Implements parallel execution with <code>asyncio</code> to optimize response times.<br /><br />
+      
+          The service supports CORS for frontend interaction and uses structured output functions to return validated post suggestions and analysis.
+        `, 
+        "desc": "", 
+        "id": "media-14" 
+      }      
     ],
   },
   {
-    slug: "ecommerce-site",
+    slug: "workout-tracker-app",
     media: [
       { type: 'header', content: 'Feature Demonstration', desc: "h2", id: "header-0" },
-      { type: "video", content: '/videos/projects/slug/maibel_demo.mp4', desc: "Demo Video", id: "media-1" },
+      { type: "video", content: "https://www.youtube.com/embed/wrNX9XlC3xw?si=sJOZHtPXjVzo2Aog", desc: "Demo Video", id: "media-1" },
       { type: "text", content: `
-        As you can see, the application features an agent that you can speak with, challenge progression based on intent, validation
-        checks, stories, and basic theme switching.
+        As you can see, the application features theme switching, firebase authentication and CRUD for workouts, history as well as exercises.
+        It is also able to process payment, and visualise statistics.
       `, desc: "", id: "media-2" },
-      { type: 'header', content: 'Creating the AI', desc: "h2", id: "header-3" },
-      { type: 'text', content: `
-        Let's go through<br />
-        1. the validation bot<br />
-        2. challenge progression bot<br />
-        3. The main conversational chatbot<br />
-        4. Custom Avatar creation and how that affects the behavior of the chatbot
-      `, desc: "", id: "media-4" },
-      { type: 'header', content: 'Validation Bot', desc: "h3", id: "header-5" },
-      { type: 'header', content: 'Challenge Bot', desc: "h3", id: "header-6" },
-      { type: 'header', content: 'Conversational Bot', desc: "h3", id: "header-7" },
-      { type: 'header', content: 'Custom Avatars', desc: "h3", id: "header-8" },
-      { type: 'header', content: 'Deployment', desc: "h2", id: "header-9" },
-      { type: 'image', content: '/images/projects/slug/apple_developer_store.jpg', desc: "Apple Developer Store", id: "media-10" },
-      { type: 'image', content: '/images/projects/slug/play_store.jpg', desc: "Google Play Store", id: "media-11" },
-      { type: 'text', content: `
-        As shown above, I've deployed the application for internal testing to a live audience of roughly 15 people in total. These
-        were testers for Maibel's user testing session, and I managed to get the build up in just a week, with no prior experience
-        whatsoever. Let's check out the demo below!
-      `, desc: "", id: "media-12" },
     ],
   },
 ];
